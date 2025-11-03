d8b432cae952d9aeae57815383a01ce676c14cc3
jsyoon27 <tjddbswkd1@naver.com>
2025-11-03 23:59:03 +0900

    1103

--- function-demo.js
+++ function-demo.js
@@ -1,9 +1,42 @@
-function foo(arg){
-    return arg;
+// function foo(arg){
+//     arg();
+// }
+
+// function bar() {
+//     console.log('bar');
+// }
+
+// foo(bar);
+// function foo(arg){
+//     return arg;
+// }
+
+// function bar() {
+//     console.log('bar');
+// }
+
+// foo(bar)();
+
+// const foo = function (arg){
+//     return arg;
+// };
+
+// foo(1);
+
+// function foo(arg = 1) {
+//     console.log(arg);
+// }
+
+// foo();
+
+// function foo(arg, ...rest) {
+//     console.log(rest);
+// }
+
+// foo(1,2,3,4);
+
+function foo(arg) {
+    console.log(arguments);
 }
 
-function bar() {
-    console.log('bar');
-}
-
-foo(bar)();
+foo(1,2,3,4);
