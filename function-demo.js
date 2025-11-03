f88e53774008c1b7a2576d4cd470ab5f4337efdf
jsyoon27 <tjddbswkd1@naver.com>
2025-11-04 00:09:57 +0900

    1103

--- function-demo.js
+++ function-demo.js
@@ -1,3 +1,7 @@
+// - 함수의 실제 매개변수가 될 수 있다.
+// - 함수의 반환값이 될 수 있다
+// - 할당 명령문의 대상이 될 수 있다.
+// - 동일비교의 대상이 될 수 있다.
 // function foo(arg){
 //     arg();
 // }
@@ -35,8 +39,61 @@
 
 // foo(1,2,3,4);
 
-function foo(arg) {
-    console.log(arguments);
-}
+// function foo(arg) {
+//     console.log(arguments);
+// }
 
-foo(1,2,3,4);
+// foo(1,2,3,4);
+
+
+//함수 생성 방법
+
+// function foo(){
+//     console.log('foo');
+// }
+
+// foo();
+
+// const foo =function (){
+//     console.log('foo2');
+// }
+
+// foo();
+
+// const foo = new Function ("console.log('foo3')")
+// foo();
+
+// const foo = () => {
+//     console.log('foo4');
+// };
+// foo();
+
+//IIFE
+//재귀함수
+//중첩함수
+//콜백함수
+
+// (function foo() {
+//     console.log('foo');
+// })()
+
+// function foo(arg) {
+//     if(arg === 3) return;
+//     console.log(arg);
+
+//     foo(arg + 1);
+// }
+// foo(1)
+// function foo(arg) {
+// function bar(){
+//     console.log(arg);
+//     }
+//     bar();
+// }
+// foo(1)
+function foo(arg){
+    arg();
+}
+foo(()=>{
+    console.log(1);
+})
